6f266adb9a490d49ef955d23a0a17dac8b13d6d4
Zach Overhulser <zoverhulser@gmail.com>
2019-04-19 17:43:26 +0000

    Adds smurfs.

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,16 +1,45 @@
 import React, {useState} from "react";
+import {connect} from "react-redux";
 
-export default function (props) {
-	const [state, setState] = useState("");
+import {addSmurf} from "../actions";
+
+function SmurfForm(props) {
+	const [state, setState] = useState({name: "", age: "", height: ""});
+	function handleChange(e) {
+		setState({...state, [e.target.name]: e.target.value});
+	}
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (state.name && state.age && state.height) {
+			props.addSmurf(state);
+		}
+		if (!state.name || !state.age || !state.height) {
+			alert("All smurf fields required...smurf smurf");
+		}
+	}
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<input 
 				type="text"
-				name="smurf"
-				value={state}
-				onChange={({target: {value}}) => setState(value)}
+				name="name"
+				value={state.name}
+				onChange={handleChange}
+			/>
+			<input 
+				type="text"
+				name="age"
+				value={state.age}
+				onChange={handleChange}
+			/>
+			<input 
+				type="text"
+				name="height"
+				value={state.height}
+				onChange={handleChange}
 			/>
 			<button type="submit">Submit</button>
 		</form>
 	)
 }
+
+export default connect(() => {}, {addSmurf})(SmurfForm);
